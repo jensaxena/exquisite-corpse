2a1a5dbbaf85dbe773db6b6c4bc31206532ac180
Jen Saxena <jen@jensaxena.com>
2018-06-09 22:47:12 -0400

    Add Main component and styles

---
+++ src/Main.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './Main.css'
+
+export default class Main extends Component {
+  render() {
+    return (
+      <main className="Main">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae placeat expedita incidunt sit quis non dolores est dolorum doloribus sed ullam delectus unde maiores, alias eum odit. Cupiditate, ad sint!</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae placeat expedita incidunt sit quis non dolores est dolorum doloribus sed ullam delectus unde maiores, alias eum odit. Cupiditate, ad sint!</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae placeat expedita incidunt sit quis non dolores est dolorum doloribus sed ullam delectus unde maiores, alias eum odit. Cupiditate, ad sint!</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae placeat expedita incidunt sit quis non dolores est dolorum doloribus sed ullam delectus unde maiores, alias eum odit. Cupiditate, ad sint!</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae placeat expedita incidunt sit quis non dolores est dolorum doloribus sed ullam delectus unde maiores, alias eum odit. Cupiditate, ad sint!</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae placeat expedita incidunt sit quis non dolores est dolorum doloribus sed ullam delectus unde maiores, alias eum odit. Cupiditate, ad sint!</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae placeat expedita incidunt sit quis non dolores est dolorum doloribus sed ullam delectus unde maiores, alias eum odit. Cupiditate, ad sint!</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae placeat expedita incidunt sit quis non dolores est dolorum doloribus sed ullam delectus unde maiores, alias eum odit. Cupiditate, ad sint!</p>
+
+      </main>
+    )
+  }
+}
