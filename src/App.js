13bfaa08e49fc40b0f3ebf1629374734f683e95e
Jen Saxena <jen@jensaxena.com>
2018-06-09 22:48:00 -0400

    Add App styles, import content components

--- src/App.js
+++ src/App.js
@@ -1,53 +1,17 @@
 import React, { Component } from 'react'
-import './App.sass'
+import Header from './Header'
+import Main from './Main'
+import Footer from './Footer'
+import './App.css'
 
-let styles1 = {
-  width: '150px',
-  height: '150px',
-  margin: '10px',
-  display: 'inline-block',
-  backgroundColor: '#57652a'
-}
-let styles2 = {
-  width: '150px',
-  height: '150px',
-  margin: '10px',
-  display: 'inline-block',
-  backgroundColor: '#dfdce3'
-}
-let styles3 = {
-  width: '150px',
-  height: '150px',
-  margin: '10px',
-  display: 'inline-block',
-  backgroundColor: '#262228'
-}
-let styles4 = {
-  width: '150px',
-  height: '150px',
-  margin: '10px',
-  display: 'inline-block',
-  backgroundColor: '#b82601'
-}
-let styles5 = {
-  width: '150px',
-  height: '150px',
-  margin: '10px',
-  display: 'inline-block',
-  backgroundColor: '#dcc7aa'
-}
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <div style={styles1}></div>
-        <div style={styles2}></div>
-        <div style={styles3}></div>
-        <div style={styles4}></div>
-        <div style={styles5}></div>
+        <Header />
+        <Main />
+        <Footer />
       </div>
     )
   }
 }
-
-export default App
