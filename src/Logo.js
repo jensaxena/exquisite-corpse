4c9f5d4358266202adece6deb6825f6b5772fd84
Jen Saxena <jen@jensaxena.com>
2018-06-09 22:46:38 -0400

    Add Logo component and styles

---
+++ src/Logo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Logo.css'
+
+export default class Logo extends Component {
+  render() {
+    return (
+      <div className="Logo">
+        <h1>Exquisite Corpse</h1>
+      </div>
+    )
+  }
+}
